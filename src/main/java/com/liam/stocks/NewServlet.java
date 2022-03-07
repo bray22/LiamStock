8f1141f1ebffd6351d53fb04fdbbba0dd2c125fe
Ben Ray <bray22@gmail.com>
2022-03-07 18:15:59 -0500

    Initial Commit

---
+++ src/main/java/com/liam/stocks/NewServlet.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.liam.stocks;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author benray
+ */
+public class NewServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+       // String price = request.getParameter("price");
+       // int parameter = Integer.parseInt(price);
+        
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewServlet at..." + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+              StockBean eObj = new StockBean();
+       
+        eObj.setPrice(2);
+        eObj.setStockname(name);
+       
+ 
+        /**** Storing Bean In Session ****/
+        request.getSession().setAttribute("emp", eObj);
+ 
+        RequestDispatcher rd = request.getRequestDispatcher("/Stock.jsp");
+        rd.forward(request, response);
+           // response.sendRedirect("./Stock.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    
+    
+    public int getWebService() {
+        //https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo
+        return 5;
+    }
+
+}
---
+++ src/main/java/com/liam/stocks/StockBean.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatefulEjbClass.java to edit this template
+ */
+package com.liam.stocks;
+
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author benray
+ */
+@Stateful
+public class StockBean {
+
+    private int price;
+    private String stockname;
+   
+    public int getPrice() {
+        return price;
+    }
+    public int setPrice(int price) {
+        this.price = price;
+        return 12;
+    }
+    public String getStockname() {
+        return stockname;
+    }
+    public void setStockname(String stockname) {
+        this.stockname = stockname;
+    }
+     
+    @Override
+    public String toString() {
+        return "SampleBean [param1=" + price + ", param2=" + stockname + "]";
+    }
+}
